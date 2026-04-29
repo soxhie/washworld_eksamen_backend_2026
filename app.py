54d9d68a90bad39fd0cef26502572c2fa6eced60
sophi <sophi@example.com>
2026-04-29 13:39:48 +0200

    Implement user signup functionality with validation and create signup HTML form

--- app.py
+++ app.py
@@ -22,3 +22,58 @@ app.config['SESSION_TYPE'] = 'filesystem'
 Session(app)
 app.config["JWT_SECRET_KEY"] = "passwordpasswordpasswordpassword"
 jwt = JWTManager(app)
+
+
+##############################
+@app.get("/")
+def index():
+    return jsonify({"status":"ok", "message":"Connected"})
+##############################
+@app.post("/signup")
+def signup():
+    try:
+        user_id = str(uuid.uuid4())
+        user_name = x.validate_user_name(request.json.get("user_name", ""))
+        user_last_name = x.validate_user_last_name(request.json.get("user_last_name", ""))
+        user_address = x.validate_user_address(request.json.get("user_address", ""))
+        user_phone = x.validate_user_phone(request.json.get("user_phone", ""))
+        user_email = x.validate_email(request.json.get("user_email", ""))
+        user_payment_gateway_fk = x.validate_id(request.json.get("user_payment_gateway_fk", ""))
+        user_password = x.validate_user_password(request.json.get("user_password", ""))
+        user_password_hashed = generate_password_hash(user_password)
+        user_created_at = int(time.time())
+
+         
+        
+        db, cursor = x.db()
+        q = "INSERT INTO users VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s) "
+        cursor.execute(q, (user_id, user_name, user_last_name, user_email, user_hashed_password, created_at, user_address, user_phone, user_payment_gateway_fk))
+        db.commit()
+    except Exception as ex:
+       
+        if "company_exception user_email" in str(ex):
+            error_message = "email invalid"
+            return jsonify({"status": "error", "message": error_message}), 400
+        
+        if "company_exception user_password" in str(ex):
+            error_message = f"user password {x.USER_PASSWORD_MIN} to {x.USER_PASSWORD_MAX} characters"
+            return jsonify({"status": "error", "message": error_message}), 400
+        
+        if "company_exception user_role" in str(ex):
+            error_message = f"user role {x.USER_ROLE_MIN} to {x.USER_ROLE_MAX} characters"
+            return jsonify({"status": "error", "message": error_message}), 400
+        
+        if "Duplicate entry" in str(ex) and "user_email" in str(ex):
+            error_message = "You already have an account with this email, please or use another email"
+            return jsonify({"status": "error", "message": error_message}), 400
+        # Worst case
+        error_message = "System under maintenance"
+        return jsonify({"status": "error", "message": str(ex)}), 500
+    finally:
+        if "cursor" in locals(): cursor.close()
+        if "db" in locals(): db.close()
+##############################
+@app.get("/signup")
+def show_signup():
+    return render_template("page_signup.html")
+##############################
--- x.py
+++ x.py
@@ -38,8 +38,99 @@ def no_cache(view):
         response.headers["Expires"] = "0"
         return response
     return no_cache_view
-
-
 #################
 def format_epoch_date(epoch_value):
     return datetime.fromtimestamp(epoch_value).strftime("%Y-%m-%d")
+##############################
+USER_NAME_MIN = 2
+USER_NAME_MAX = 20
+REGEX_USER_NAME = f"^.{{{USER_NAME_MIN},{USER_NAME_MAX}}}$"
+def validate_user_name( user_name ):
+    user_name = user_name.strip()
+    if not re.match(REGEX_USER_NAME, user_name):
+        raise Exception("company_exception user_name")
+    return user_name
+
+
+##############################
+USER_LAST_NAME_MIN = 2
+USER_LAST_NAME_MAX = 20
+REGEX_USER_LAST_NAME = f"^.{{{USER_LAST_NAME_MIN},{USER_LAST_NAME_MAX}}}$"
+def validate_user_last_name(user_last_name):
+    user_last_name = user_last_name.strip()
+    if not re.match(REGEX_USER_LAST_NAME, user_last_name):
+        raise Exception("company_exception user_last_name")
+    return user_last_name
+
+
+##############################
+REGEX_EMAIL = "^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$"
+def validate_email( email ):
+    email = email.strip()
+    if not re.match(REGEX_EMAIL, email): 
+        raise Exception("company_exception email")
+    return email
+##############################
+REGEX_USER_ADRESS = "/^(\d{1,}) [a-zA-Z0-9\s]+(\,)? [a-zA-Z]+(\,)? [A-Z]{2} [0-9]{5,6}$/"
+def validate_user_adress(user_adress):
+    user_adress = user_adress.strip()
+    if not re.match(REGEX_USER_ADRESS, user_adress):
+        raise Exception("company_exception user_adress")
+    return user_adress
+##############################
+REGEX_USER_PHONE = "^(\+45)?\s?(\d{2}\s?){4}$"
+def validate_user_phone(user_phone):
+    user_phone = user_phone.strip()
+    if not re.match(REGEX_USER_PHONE, user_phone):
+        raise Exception("company_exception user_phone")
+    return user_phone
+##############################
+USER_PASSWORD_MIN = 8
+USER_PASSWORD_MAX = 50
+
+REGEX_USER_PASSWORD = f"^(?=.*[a-z])(?=.*[A-Z])(?=.*[\d\W]).{{8,}}$"
+def validate_user_password( password ):
+    user_password = password.strip()
+    if not re.match(REGEX_USER_PASSWORD, user_password):
+        raise Exception("company_exception user_password")
+    return user_password
+
+##############################
+REGEX_ID = "^[a-f0-9]{32}$"
+def validate_id(id):
+    id = id.strip()
+    if not re.match(REGEX_ID, id):
+        raise Exception("company_exception id")
+    return id
+##############################
+USER_ROLE_MIN = 2
+USER_ROLE_MAX = 20
+REGEX_USER_ROLE = f"^.{{{USER_ROLE_MIN},{USER_ROLE_MAX}}}$"
+def validate_user_role():
+    user_role = request.form.get("user_role", "").strip()
+    if not re.match(REGEX_USER_ROLE, user_role):
+        raise Exception("company_exception user_role")
+    return user_role
+##############################
+# You know that the PK is a uuid4
+# uuid4 follows certain patterns
+#  TODO: replace game_pk and user_pk in the forms 
+
+##############################
+# 0 to 9 letters a to f
+REGEX_UUID4 = "^[0-9a-f]{32}$"
+def validate_uuid4(uuid4):
+    uuid = uuid4.strip()
+    if not re.match(REGEX_UUID4, uuid):
+        raise Exception("company_exception uuid4 invalid")
+    return uuid
+
+##############################
+REGEX_PARANOID = "^[0-9a-f]{64}$"
+def validate_uuid4_paranoia(uuid4):
+    uuid = uuid4.strip()
+    if not re.match(REGEX_PARANOID, uuid):
+        raise Exception("company_exception paranoia")
+    return uuid
+
+
